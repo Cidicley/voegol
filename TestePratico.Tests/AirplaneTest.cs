0af9c1f66079856246a8e5d7ff3d2db33d9aa57d
Cidicley Rodrigues <33094500+Cidicley@users.noreply.github.com>
2019-10-25 13:17:00 -0300

    Testes unitátios
    Inclusão do xUnit para realizar os testes unitários.

---
+++ TestePratico.Tests/AirplaneTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TestePratico.API.domain.Models;
+using TestePratico.API.domain.Services;
+using TestePratico.API.Services;
+using Xunit;
+
+namespace TestePratico.Tests
+{
+    public class AirplaneTest
+    {
+        private readonly IAirplaneService _airplaneService;
+
+        public AirplaneTest(IAirplaneService airplaneService)
+        {
+            _airplaneService = airplaneService;
+        }
+
+        [Fact]
+        public async Task GetAllAirplaneAsync()
+        {
+            //Arranje - Cenário            
+
+
+            //Act - método de teste
+            IEnumerable<Airplane> retorno = await _airplaneService.GetAllAirplaneAsync();
+
+            Assert.NotEmpty(retorno);
+        }
+    }
+}

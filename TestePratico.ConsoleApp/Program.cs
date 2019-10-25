8900f7aeaa8cadbb845818f9f0495291fffa5b27
Cidicley Rodrigues <33094500+Cidicley@users.noreply.github.com>
2019-10-25 13:18:39 -0300

    Testes unitários
    Iniciando os testes unitários.

---
+++ TestePratico.ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TestePratico.API.domain.Models;
+using TestePratico.API.domain.Services;
+
+namespace TestePratico.ConsoleApp
+{
+    class Program
+    {
+        private static readonly IAirplaneService _airplaneService;
+
+        //public Program(IAirplaneService airplaneService)
+        //{
+        //    _airplaneService = airplaneService;
+        //}
+
+        private static async Task GetAllAirplaneAsync()
+        {
+            //Arranje - Cenário
+            var boing = new Airplane { Id = 1000, Nome = "Avião teste" };
+
+            //Act - método de teste
+            IEnumerable<Airplane> retorno = await _airplaneService.GetAllAirplaneAsync();
+
+
+            Console.WriteLine("teste");
+        }
+
+        static void Main()
+        {
+            GetAllAirplaneAsync();
+
+        }
+    }
+}
